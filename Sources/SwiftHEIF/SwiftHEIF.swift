23acf69993b74b86dae3ca649814929636689fdf
Greg Fajen <gregfajen@gmail.com>
2019-11-13 17:04:31 -0500

    more types, started moving over swift

--- Sources/SwiftHEIF/SwiftHEIF.swift
+++ Sources/SwiftHEIF/SwiftHEIF.swift
@@ -5,12 +5,47 @@
 //  Created by Greg Fajen on 11/13/19.
 //
 
-#if os(Linux)
-import Glibc
-#else
-import Darwin
-#endif
-
 import Foundation
 import libheif
 
+class HEIFContext {
+    
+    let context: OpaquePointer
+    
+    init() {
+        context = heif_context_alloc()
+    }
+    
+    deinit {
+        heif_context_free(context)
+    }
+    
+}
+
+class HEIFEncoder {
+    
+    var encoder: heif_encoder_ptr
+    
+    init?(context: HEIFContext) {
+        var comp = heif_compression_HEVC
+        let pointer = OpaquePointer(UnsafeRawPointer(&comp))
+        
+        let result = heif_context_get_encoder2(context.context, pointer)
+        
+        let error = result.error
+        print("error: \(error.code) \(error.subcode)")
+        
+        if let encoder = result.encoder {
+            self.encoder = encoder
+        } else {
+            return nil
+        }
+    }
+    
+    var lossyQuality: Int = 100 {
+        didSet {
+            heif_encoder_set_lossy_quality(encoder, Int32(lossyQuality))
+        }
+    }
+    
+}
--- Sources/libheif/libheif.h
+++ Sources/libheif/libheif.h
@@ -15,3 +15,20 @@
 #endif
 
 typedef struct heif_encoder *heif_encoder_ptr;
+
+typedef struct {
+    heif_encoder_ptr encoder;
+    struct heif_error error;
+} heif_encoder_result;
+
+heif_encoder_result heif_context_get_encoder2(struct heif_context* context,
+                                              const struct heif_encoder_descriptor* encoding) {
+    heif_encoder_ptr encoder;
+    struct heif_error error = heif_context_get_encoder(context, encoding, &encoder);
+    
+    heif_encoder_result result;
+    result.encoder = encoder;
+    result.error = error;
+    
+    return result;
+}
